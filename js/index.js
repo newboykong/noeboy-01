3c547eac6a075eba54b66de4b3dc56d22bdce015
newboykong <111245711+newboykong@users.noreply.github.com>
2022-08-15 03:54:45 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function validateForm(){
+    var x=document.forms["myForm"]["fname"].value
+    if(x==null||x==""){
+        alert("必需填写");
+    }
+    else if(x=="王志远"){
+        window.open("./html/index02.html")
+    }
+    else{
+        alert("你写错了");
+    }
+}
